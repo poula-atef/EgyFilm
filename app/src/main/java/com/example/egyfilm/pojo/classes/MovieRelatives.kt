2a406e670aa614bb276cce468d287d76eaa54e45
PoulaAtef <bebobob223@hmail.com>
2021-10-24 22:06:05 +0200

    Get Similar Movies For A Movie Is Added

---
+++ app/src/main/java/com/example/egyfilm/pojo/classes/MovieRelatives.kt
@@ -0,0 +1,11 @@
+package com.example.egyfilm.pojo.classes
+
+import com.google.gson.annotations.SerializedName
+
+
+data class MovieRelatives(
+    @SerializedName("total_pages") val totalPages : Int,
+    @SerializedName("total_results") val totalResults : Int,
+    val page : Int,
+    val results : List<Movie>
+)
